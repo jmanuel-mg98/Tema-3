fd4dba5c02dbfcf2d949f8776f8f9bc8d48daa44
josem.martin <josem.martin@106-21.info2>
2023-11-22 09:17:59 +0100

    ejercise1

---
+++ src/ejercise1/EcoEjercise1.java
@@ -0,0 +1,12 @@
+package ejercise1;
+
+public class EcoEjercise1 {
+	
+	public static void ecoEjercise1(int num) {
+		
+		for(int i=0;i<num;i++) {
+			System.out.println(num);
+		}
+	
+	}
+}
---
+++ src/ejercise1/Ejercise1Main.java
@@ -0,0 +1,18 @@
+package ejercise1;
+
+import java.util.Scanner;
+
+public class Ejercise1Main {
+
+	public static void main(String[] args) {
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("introduce un numero: ");
+		num= sc.nextInt();
+		EcoEjercise1.ecoEjercise1(num);
+		
+		
+
+	}
+
+}
